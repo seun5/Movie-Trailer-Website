a19f1a825fdf757411d245965b5c21aae0b8e809
seun5 <seun5@emory.edu>
2018-01-17 15:10:33 +0900

    Update media.py

--- media.py
+++ media.py
@@ -1,7 +1,7 @@
 import requests
 
 # These are global variables that are needed to get the tMDB API response
-api_main =  'https://api.themoviedb.org/3/movie/'
+api_main = 'https://api.themoviedb.org/3/movie/'
 api_video = 'https://www.youtube.com/watch?v='
 api_image = 'http://image.tmdb.org/t/p/w342///'
 api_key = '?api_key=80555f22c7835f1b06f5da898a041d5e'
@@ -16,9 +16,10 @@ class Movie():
         video_url = api_main + movie_id + "/videos" + api_key
         # Movie Data in JSON format
         json_data = requests.get(url).json()
-        json_trailer = requests.get(video_url).json()
+        json_vid = requests.get(video_url).json()
         # Get official(original) movie Title, Poster
         self.title = json_data['original_title']
         self.poster_image_url = api_image + json_data["poster_path"]
         # Since multiple trailers are available,just choose the first one
-        self.trailer_youtube_url = api_video + json_trailer["results"][0]["key"]
+        self.trailer_youtube_url = api_video + json_vid["results"][0]["key"]
+        
